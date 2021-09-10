edfb7ecbf3b39b7115c4633aee4131460ea01cb9
Mike Tashuk <mike120899@gmail.com>
2021-09-10 19:44:07 +0300

    rename files

--- test/unit_test/models/crypto_currency_model_test.dart
+++ test/unit_test/models/crypto_currency_model_test.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-import 'package:crypto_currency_tracker/src/app/data/models/crypto_coin_model.dart';
+import 'package:crypto_currency_tracker/src/app/data/models/crypto_currency_model.dart';
 import 'package:crypto_currency_tracker/src/app/domain/entities/crypto_currency.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -19,7 +19,7 @@ void main() {
     test("should return a valid model when the JSON number is an integer",
         () async {
       final Map<String, dynamic> jsonMap =
-          json.decode(fixture('crypto_coin.json'));
+          json.decode(fixture('crypto_currency.json'));
 
       final result = CryptoCurrencyModel.fromJson(jsonMap);
       expect(result, cryptoCurrencyModel);
@@ -28,7 +28,7 @@ void main() {
     test("should return a valid model when the JSON number is an double",
         () async {
       final Map<String, dynamic> jsonMap =
-          json.decode(fixture('crypto_coin_double.json'));
+          json.decode(fixture('crypto_currency_double.json'));
 
       final result = CryptoCurrencyModel.fromJson(jsonMap);
       expect(result, cryptoCurrencyModel);
