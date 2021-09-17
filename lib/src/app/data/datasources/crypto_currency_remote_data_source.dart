36434dc1301463da8ec7985fbe547568db4a799b
Тащук Михаил <mike120899@gmail.com>
2021-09-17 03:33:24 +0300

    finish remote data source implementation

--- lib/src/app/data/datasources/crypto_currency_remote_data_source.dart
+++ lib/src/app/data/datasources/crypto_currency_remote_data_source.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:crypto_currency_tracker/src/app/data/models/crypto_currency_model.dart';
+import 'package:crypto_currency_tracker/src/core/error/exception.dart';
 import 'package:http/http.dart';
 
 abstract class CryptoCurrencyRemoteDataSource {
@@ -11,28 +15,62 @@ abstract class CryptoCurrencyRemoteDataSource {
 
 class CryptoCurrencyRemoteDataSourceImpl
     implements CryptoCurrencyRemoteDataSource {
-  static const url =
+  static const baseUrl =
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd";
 
-  final Client httpClient;
+  final Client client;
 
-  CryptoCurrencyRemoteDataSourceImpl(this.httpClient);
+  CryptoCurrencyRemoteDataSourceImpl(this.client);
 
   @override
-  Future<List<CryptoCurrencyModel>> getCryptoCurrencyArray(List<String> ids) {
-    // TODO: implement getCryptoCurrencyArray
-    throw UnimplementedError();
+  Future<List<CryptoCurrencyModel>> getCryptoCurrencyArray(List<String> ids) async {
+    var responce = await client.get(cryptoCurrencyArrayInfoUrl(ids));
+
+    if (responce.statusCode != HttpStatus.ok) {
+      throw ServerException();
+    }
+
+    List json = jsonDecode(responce.body);
+
+    return json.map((e) => CryptoCurrencyModel.fromJson(e)).toList();
   }
 
   @override
-  Future<CryptoCurrencyModel> getCryptoCurrencyInfo(String id) {
-    // TODO: implement getCryptoCurrencyInfo
-    throw UnimplementedError();
+  Future<CryptoCurrencyModel> getCryptoCurrencyInfo(String id) async {
+    var responce = await client.get(cryptoCurrencyInfoUrl(id));
+
+    if (responce.statusCode != HttpStatus.ok || responce.body == "[]") {
+      throw ServerException();
+    }
+
+    List json = jsonDecode(responce.body);
+
+    return CryptoCurrencyModel.fromJson(json.first);
   }
 
   @override
-  Future<List<CryptoCurrencyModel>> getTopCryptoCurrencies() {
-    // TODO: implement getTopCryptoCurrencies
-    throw UnimplementedError();
+  Future<List<CryptoCurrencyModel>> getTopCryptoCurrencies() async {
+    var responce = await client.get(topCryptoCurrenciesUrl());
+
+    if (responce.statusCode != HttpStatus.ok) {
+      throw ServerException();
+    }
+
+    List json = jsonDecode(responce.body);
+
+    return json.map((e) => CryptoCurrencyModel.fromJson(e)).toList();
+  }
+
+//TODO: take out to service
+  Uri topCryptoCurrenciesUrl() {
+    return Uri.parse("$baseUrl&per_page=20");
+  }
+
+  Uri cryptoCurrencyInfoUrl(String id) {
+    return Uri.parse("$baseUrl&ids=$id");
+  }
+
+  Uri cryptoCurrencyArrayInfoUrl(List<String> ids) {
+    return Uri.parse("$baseUrl&ids=${ids.join(",")}");
   }
 }
--- test/unit_test/app/data/datasources/crypto_currency_remote_data_source_test.dart
+++ test/unit_test/app/data/datasources/crypto_currency_remote_data_source_test.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+
 import 'package:crypto_currency_tracker/src/app/data/datasources/crypto_currency_remote_data_source.dart';
 import 'package:crypto_currency_tracker/src/app/data/models/crypto_currency_model.dart';
+import 'package:crypto_currency_tracker/src/core/error/exception.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 import 'package:mocktail/mocktail.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
 
 class MockHttpClient extends Mock implements Client {}
 
@@ -15,11 +20,18 @@ void main() {
     dataSource = CryptoCurrencyRemoteDataSourceImpl(mockHttpClient);
   });
 
-  final url = CryptoCurrencyRemoteDataSourceImpl.url;
+  final topCryptoCurrencyUrl = Uri.parse(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=20");
+  final cryptoCurrencyInfoUrl = Uri.parse(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin");
+  final cryptoCurrencyArrayInfoUrl = Uri.parse(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin,ethereum");
 
-  const List<String> ids = ["bitcoin", "ethereum"];
   const id = "bitcoin";
+  const List<String> ids = ["bitcoin", "ethereum"];
 
+  const CryptoCurrencyModel cryptoCurrencyModel = CryptoCurrencyModel(
+      "bitcoin", "Bitcoin", "BTC", "imageUrl", 45000, 1, 1500, 1);
   const List<CryptoCurrencyModel> cryptoCurrencyModels = [
     CryptoCurrencyModel(
         "bitcoin", "Bitcoin", "BTC", "imageUrl", 45000, 1, 1500, 1),
@@ -30,7 +42,75 @@ void main() {
   group("getCryptoCurrencyArray", () {
     test("should return array of crypto currencies fetched from server",
         () async {
-          var 
-        });
+      when(() => mockHttpClient.get(topCryptoCurrencyUrl)).thenAnswer(
+          (_) async =>
+              Response(fixture("crypto_currencies.json"), HttpStatus.ok));
+
+      var result = await dataSource.getTopCryptoCurrencies();
+
+      expect(result, cryptoCurrencyModels);
+
+      verify(() => mockHttpClient.get(topCryptoCurrencyUrl));
+    });
+
+    test("should throw server excepiton on error status code", () async {
+      when(() => mockHttpClient.get(topCryptoCurrencyUrl))
+          .thenAnswer((_) async => Response("", HttpStatus.badRequest));
+
+      expect(() async => await dataSource.getTopCryptoCurrencies(),
+          throwsA(const TypeMatcher<ServerException>()));
+
+      verify(() => mockHttpClient.get(topCryptoCurrencyUrl));
+    });
+  });
+
+  group("getCryptoCurrencyInfo", () {
+    test("should return crypto currency info by id", () async {
+      when(() => mockHttpClient.get(cryptoCurrencyInfoUrl)).thenAnswer(
+          (_) async => Response(
+              fixture("crypto_currency_responce.json"), HttpStatus.ok));
+
+      var result = await dataSource.getCryptoCurrencyInfo(id);
+
+      expect(result, cryptoCurrencyModel);
+
+      verify(() => mockHttpClient.get(cryptoCurrencyInfoUrl));
+
+      verifyNoMoreInteractions(mockHttpClient);
+    });
+
+    test("should throw exception on error code", () {
+      when(() => mockHttpClient.get(cryptoCurrencyInfoUrl))
+          .thenAnswer((_) async => Response("", HttpStatus.badRequest));
+
+      expect(() async => await dataSource.getCryptoCurrencyInfo(id),
+          throwsA(const TypeMatcher<ServerException>()));
+
+      verify(() => mockHttpClient.get(cryptoCurrencyInfoUrl));
+    });
+  });
+
+  group("getCryptoCurrencyArray", () {
+    test("should return array of crypto currency", () async {
+      when(() => mockHttpClient.get(cryptoCurrencyArrayInfoUrl)).thenAnswer(
+          (_) async =>
+              Response(fixture("crypto_currencies.json"), HttpStatus.ok));
+
+      var result = await dataSource.getCryptoCurrencyArray(ids);
+
+      expect(result, cryptoCurrencyModels);
+
+      verify(() => mockHttpClient.get(cryptoCurrencyArrayInfoUrl));
+    });
+
+    test("should throw exception on fetching info", () async {
+      when(() => mockHttpClient.get(cryptoCurrencyArrayInfoUrl))
+          .thenAnswer((_) async => Response("", HttpStatus.badRequest));
+
+      expect(() async => await dataSource.getCryptoCurrencyArray(ids),
+          throwsA(const TypeMatcher<ServerException>()));
+
+      verify(() => mockHttpClient.get(cryptoCurrencyArrayInfoUrl));
+    });
   });
 }
